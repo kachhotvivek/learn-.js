2d53d79f7289a2588f0a1021a3a691ac4f0a788b
Kachhot vivek <kachhotvivek18@gmail.com>
2024-10-08 17:31:06 +0530

    Add files via upload

---
+++ 01basic/01_variable.js
@@ -0,0 +1,17 @@
+// const in not change
+const accountId = 102543
+// let is change 
+let accountEmail = "vivek@gmail.com"
+
+var accountpass = "1234"
+
+accountCity = "jaipur"
+
+accountId = 2
+
+accountEmail = "jay"
+accountpass = "9090"
+accountCity = "gir"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpass,accountCity])

561fa809b957b9bbfbb75518cd1a8f1a80b54171
Kachhot vivek <kachhotvivek18@gmail.com>
2024-10-08 17:38:25 +0530

    Update 01_variable.js

--- 01basic/01_variable.js
+++ 01basic/01_variable.js
@@ -1,14 +1,18 @@
 // const in not change
 const accountId = 102543
+
 // let is change 
 let accountEmail = "vivek@gmail.com"
 
+// var is not use but this value is change 
 var accountpass = "1234"
 
 accountCity = "jaipur"
 
-accountId = 2
+// try to value change
+//accountId = 2
 
+// value change for let,var and not vaeriable find
 accountEmail = "jay"
 accountpass = "9090"
 accountCity = "gir"

91bc2bbd815477f9de3dab05159e7bc65c713b4c
Kachhot vivek <kachhotvivek18@gmail.com>
2024-10-09 13:48:54 +0530

    Add files via upload

---
+++ 01basic/02_datatypes.js
@@ -0,0 +1,27 @@
+// data tyapes and ECMA Standards
+
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 + 4)
+console.log("vivek")
+
+let name = "vivek"
+let age = 19
+let isloggedin = false
+let satate;
+
+
+// number -> 2 to power 53
+// bigint 
+// string -> use for ""
+// boolean -> true /false
+// null -> standalone value
+// undefined 
+// symbol -> unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object

dc8f62542972e28d33451b079f3e9794f3c070fc
Kachhot vivek <kachhotvivek18@gmail.com>
2024-10-11 08:31:40 +0530

    Add files via upload

---
+++ 01basic/05_strings.js
@@ -0,0 +1,21 @@
+const name = "vivek"
+const recount = 55
+
+console.log(`hello my name is ${name} and my repo count is ${recount}`);
+
+const gamename = new String('kachhot')
+
+// console.log(gamename[4]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('k'));
+
+// const newstring = gamename.substring(0 , 3)
+// console.log(newstring);
+
+const anotherstring = gamename.slice(-5,-2)
+console.log(anotherstring);
+

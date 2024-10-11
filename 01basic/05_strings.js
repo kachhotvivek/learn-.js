9781e376dfddc7c4232ae3c048bbe2948d74d39c
Kachhot vivek <kachhotvivek18@gmail.com>
2024-10-11 15:25:07 +0530

    Add files via upload

--- 01basic/05_strings.js
+++ 01basic/05_strings.js
@@ -19,3 +19,26 @@ const gamename = new String('kachhot')
 const anotherstring = gamename.slice(-5,-2)
 console.log(anotherstring);
 
+//strating and and  space delet and show to output
+const newstrings = '    vivek      .     '
+console.log(newstrings);
+console.log(newstrings.trim());
+
+//ending space delete  
+console.log(newstrings.trimEnd());
+
+// starting space delete
+console.log(newstrings.trimStart());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+//url value change use to replace
+console.log(url.replace('%20', '-'))
+
+//string in find the value this value is aveleable and not aveleable 
+console.log(url.includes('hitesh'))
+
+// string under words divide.
+console.log(url.split['%20',"hitesh"]);
+
+
